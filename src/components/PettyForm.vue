3c7b622ed1f98e867ad66c48f9b88ce95fc24b9f
estherab <eaburabura@gmail.com>
2018-06-27 15:54:27 +0100

    formsupdate

---
+++ src/components/PettyForm.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="row">
+        <div class="PettyForm col-md-10">
+            <p class="petty">Petty<b>Cash</b></p>
+            <p>DETAILS</p>
+            <form>
+                <div class="form-row">
+                    <div class="form-group col-md-4">
+                    <label for="Date">Date:</label>
+                    <input type="date" class="form-control" id="Date" placeholder="DD/MM/YY">
+                    </div>
+                    <div class="form-group col-md-4">
+                    <label for="Amount">Amount</label>
+                    <input type="number" class="form-control" id="Amount" placeholder="GHS">
+                    </div>
+                </div>
+                
+                
+            </form>
+        </div>
+       </div>
+
+ 
+</template>
+
+<script>
+export default {
+  name: 'PettyForm',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.PettyForm{
+    margin:auto;
+    background:#f6f6f6;
+    margin-top:50px;
+}
+
+.petty{
+    font-size:1.8em;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import PettyForm from '@/components/PettyForm'
 import login from '@/components/login'
 
 Vue.use(Router)
 
 export default new Router({
-  mode:'history',
+  mode: 'history',
   routes: [
     {
       path: '/login',
       name: 'login',
       component: login
+    },
+    {
+      path: '/PettyForm',
+      name: 'PettyForm',
+      component: PettyForm
     }
   ]
 })
