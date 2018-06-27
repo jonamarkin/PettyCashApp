c3dc14c901a82dfae425381a463e3c433201709f
Jonathan Ato Markin <jona10711@gmail.com>
2018-06-27 17:13:46 +0100

    PettyForm

--- src/components/PettyForm.vue
+++ src/components/PettyForm.vue
@@ -1,24 +1,49 @@
 <template>
-    <div class="row">
-        <div class="PettyForm col-md-10">
-            <p class="petty">Petty<b>Cash</b></p>
-            <p>DETAILS</p>
-            <form>
-                <div class="form-row">
-                    <div class="form-group col-md-4">
-                    <label for="Date">Date:</label>
-                    <input type="date" class="form-control" id="Date" placeholder="DD/MM/YY">
+    <div class="container">
+        <div class="row">
+            <div class="PettyForm col-md-10">
+                <p class="petty">Petty<b>Cash</b></p>
+                <p><b>DETAILS</b></p>
+                <form>
+                    <div class="form-row">
+                        <div class="form-group col-md-4">
+                        <label for="Date"><b>Date:</b></label>
+                        <input type="date" class="form-control" id="Date" placeholder="DD/MM/YY">
+                        </div>
+                        <div class="form-group col-md-4 amount">
+                        <label for="Amount">Amount</label>
+                        <input type="number" class="form-control" id="Amount" placeholder="GHS">
+                        </div>
                     </div>
-                    <div class="form-group col-md-4">
-                    <label for="Amount">Amount</label>
-                    <input type="number" class="form-control" id="Amount" placeholder="GHS">
+                    <div class="form-row">
+                        <div class="form-group col-md-4">
+                        <label for="Description"><b>Description:</b></label>
+                        <textarea class="form-control" id="Description" rows="5"></textarea>
+                        </div>
+                        <div class="form-group col-md-4 amount">
+                        <label for="Payment">Select Payment Method:</label>
+                        <select class="form-control" id="Payment">
+                            <option>Cash</option>
+                            <option>MTN Mobile Money</option>
+                            <option>Airtel Money</option>
+                            <option>tiGO Cash</option>
+                        </select>
+                        <div class="buttons">
+                        <button type="reset" class="btn  reset" >RESET</button>
+                        <button type="submit" class="btn  send">SEND</button>
+                        </div>
+                        </div>
+                        
                     </div>
-                </div>
-                
-                
-            </form>
+                    <div class="form-row buttons">              
+                       
+                        
+                    </div>
+                     
+                </form>
+            </div>
         </div>
-       </div>
+    </div>
 
  
 </template>
@@ -30,19 +55,45 @@ export default {
     return {
      
     }
-  }
+  },
+  methods:{
+       
+}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .PettyForm{
-    margin:auto;
-    background:#f6f6f6;
-    margin-top:50px;
+    
 }
-
 .petty{
     font-size:1.8em;
 }
+
+.container{
+    background:#f6f6f6;
+    font-family:Gotham;
+    margin:auto;
+    margin-top:50px;
+
+}
+
+.amount{
+    margin-left:300px;
+}
+
+.buttons{
+    margin-top:50px;
+}
+
+.reset{
+    background: #e52b32;
+}
+
+.send{
+    background: #30b479;
+}
+
+
 </style>
