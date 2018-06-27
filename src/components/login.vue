b0a07e8e87c6e321f0a6db1c87c73620b4b0b698
estherab <eaburabura@gmail.com>
2018-06-27 14:42:42 +0100

    login

---
+++ src/components/login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="login">
+    <h3>Petty<b>Cash</b></h3>
+
+  </div>
+       
+        
+ 
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.login{
+  vertical-align: middle;
+  margin-top: 300px;
+  margin-left:300px;
+  border: 1px solid black;
+  width:50%;
+  height:300px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import login from '@/components/login'
 
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/login',
+      name: 'login',
+      component: login
     }
   ]
 })
