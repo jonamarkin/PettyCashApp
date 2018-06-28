35ee36ff733f2e0f4e43fec8bebf738c85f38e3e
estherab <eaburabura@gmail.com>
2018-06-28 14:58:34 +0100

    dashboard

--- src/components/Userdashboard.vue
+++ src/components/Userdashboard.vue
@@ -1,39 +1,12 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+<div class="container">
+   <div class="flex-container">
+  <div>PettyCash</div>
+  <div class="buttons"
+  <div><button class="btn btn-default">New</button></div>
+  <div><img style="width:40px;" src="../assets/profile.png"></div>
+</div>
+</div>
 </template>
 <script>
 export default {
@@ -43,12 +16,32 @@ export default {
 <style scoped>
 .flex-container {
   display: flex;
- }
-
-.flex-container > div {
-  margin: 10px;
-  padding: 20px;
-  font-size: 30px;
+  background-color:  #f1f1f1;
+  border: 2px solid grey;
+   height: 50px;
+ 
 }
 
+.flex-container > div {
+   /* margin: 10px;
+  padding: 20px; */
+  font-size: 25px;
+ 
+}
+.container{
+    width: 90%;
+     margin: auto;
+     margin-top: 30px;
+}
+
+button{
+    margin-left: 700px;
+    font:30px; 
+    color: white;
+    background: orange;
+}
+img{
+    margin-left: 20px;
+}
+    
 </style>
