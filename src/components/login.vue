5b0a59fd11b1ea1930e974a7e3cb177233d66323
Jonathan Ato Markin <jona10711@gmail.com>
2018-06-28 10:03:50 +0100

    form

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="login">
-    <h3>Petty<b>Cash</b></h3>
+    <h1>Petty<b>Cash</b></h1>
+    <div class="plogin">
+      <!-- <span class="glyphicon glyphicon-user"></span> -->
+      <p><img  style="width:50px;" src="../assets/profile.png"></p>
+      <p>LOGIN</p>
+    </div>
+    <button class="btn btn-default"  @click="onLogin"><img style="width:20px;" src="../assets/google logo.jpg">Sign in with Google</button>
 
   </div>
        
         
- 
 </template>
 
 <script>
@@ -15,6 +20,11 @@ export default {
     return {
      
     }
+  },
+  methods:{
+    onLogin() {
+      this.$router.push({name: 'Userdashboard'}); 
+}
   }
 }
 </script>
@@ -22,11 +32,33 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .login{
+  text-align: center;
   vertical-align: middle;
-  margin-top: 300px;
-  margin-left:300px;
-  border: 1px solid black;
+  margin: 150px 0 800px 300px;
+  box-shadow: 3px 3px 3px 3px grey;
   width:50%;
-  height:300px;
+  height:400px;
+  font-family: 'Gotham' 'Times New Roman', Times, serif;
+  background: #f6f6f6;
 }
+h1{
+  padding-top:50px;
+}
+button{
+  margin-top:50px;
+  border: 1px solid black;
+  background: transparent;
+  outline: none;
+}
+button:hover{
+  background:#1875D2;
+  color: white;
+  border: none;
+}
+
+.plogin{
+  margin-top: 70px;
+  font-weight: bold;
+}
+
 </style>
