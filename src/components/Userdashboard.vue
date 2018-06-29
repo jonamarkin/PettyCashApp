596dd75caa7af9ac4edc663e9c47db02f574813d
Jonathan Ato Markin <jona10711@gmail.com>
2018-06-29 14:42:11 +0100

    dashboard

--- src/components/Userdashboard.vue
+++ src/components/Userdashboard.vue
@@ -5,17 +5,24 @@
       <nav class="topnav">
         <div class="nav-items">
           <span class="petty">Petty<b>Cash</b></span>
-          <button class="btn">New</button>
-          <img class="usericon" src="../assets/profile.png" width="40"/>
-          <div class="user-name">
-            Welcome. 
-             Esther
+          <div class="right-stuff">
+            <button class="btn new">New</button>
+            <img class="usericon" src="../assets/profile.png" width="40"/>
+            Welcome.
+            <a href="#"><button class="logoutbtn"><img class="logout" src="../assets/logout.png" width="40"/></button></a>
           </div>
         </div>
       </nav>
-      <nav class="sidenav">
-
+      <div class="dashboard">
+        <span class="dash">Dashboard</span>
+      </div>
+      <nav class="topash col-md-12">
+        <span class="history">REQUESTS</span>
       </nav>
+      <nav class="sidenav col-md-1">
+        
+      </nav>
+      
 
     </div>
   </div>
@@ -63,11 +70,70 @@ export default {
   margin-left:5px;
 }
 
-.user-name{
+.right-stuff{
   white-space:normal;
+  float:right;
 }
 
 .usericon{
   margin-bottom: 5px;
 }
+
+.logoutbtn{
+  margin:0;
+  padding:0;
+  border:none;
+}
+
+.logoutbtn:hover{
+  border:1px solid #cccccc;
+}
+
+.new{
+  margin-top:5px;
+  background:#ff921e;
+  color:#ffffff;
+  font-weight:bold;
+}
+
+.dashboard{
+   width:100%;
+    height:30px;
+    margin-top:5px;
+    border:1px solid #d5d5d5;
+}
+
+.dash{
+  padding-left:5px;
+  font-weight:bold;
+  font-size:1.2em;
+  color:#7157b1;
+}
+
+.sidenav{
+  background:#434446;
+  height:450px;
+  margin:0;
+  width:70px;
+}
+
+ul li{
+  
+}
+
+.topash{
+  background:#434446;
+  height:50px;
+  margin-top:5px;
+}
+
+.history{
+  color:#fbfbfb;
+  font-weight:bold;
+  font-size:2em;
+  margin-left:80px;
+  margin-top:5px;
+  padding-top:5px;
+}
+
 </style>
