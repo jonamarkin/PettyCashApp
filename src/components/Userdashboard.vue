53ad5d95bce78fe44d18df3f28fbc43a6ad674f3
Jonathan Ato Markin <jona10711@gmail.com>
2018-06-29 10:19:58 +0100

    usernav

--- src/components/Userdashboard.vue
+++ src/components/Userdashboard.vue
@@ -1,15 +1,30 @@
 <template>
 <div class="container">
-<div class="row">
-  <div class="col-md-12">
-      <div class="topnav">
-        <div class="left-side">
-          <span style="float:left;">Petty<b>Cash</b></span>
+  <div class="row">
+    <div class="col-md-12">
+      <nav class="topnav">
+        <div class="nav-items">
+          <span class="petty">Petty<b>Cash</b></span>
+          <button class="btn">New</button>
+          <img class="usericon" src="../assets/profile.png" width="40"/>
+          <div class="user-name">
+            Welcome. 
+             Esther
+          </div>
         </div>
-      </div>
+      </nav>
+      <nav class="sidenav">
+
+      </nav>
+
     </div>
   </div>
-</div>
+
+  </div> 
+
+
+
+
   
 
 
@@ -22,9 +37,37 @@ export default {
 </script>
 <style scoped>
 .topnav{
- 
+ width:100%;
+ height:50px;
+ margin-top:10px;
+ border:1px solid #d5d5d5;
  
 }
 
-    
+
+
+.container{
+
+}
+
+.petty{
+  margin-left:3px;
+  font-size:1.8em;
+  
+}
+
+
+.btn{
+  margin-bottom: 5px;
+  
+  margin-left:5px;
+}
+
+.user-name{
+  white-space:normal;
+}
+
+.usericon{
+  margin-bottom: 5px;
+}
 </style>
