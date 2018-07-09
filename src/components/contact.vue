5161fe542e96646eced1b21d63f83f151b1cd47f
estherab <eaburabura@gmail.com>
2018-07-09 11:34:32 +0100

    modals

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -55,6 +55,28 @@
                 <p @click="settings"><img src="../assets/settings.png">SETTINGS</p>
                 <p @click="help"><img src="../assets/help.png">HELP</p>
             </div>
+            <div>
+                    <b-btn v-b-modal.modalPrevent>Add contact</b-btn>
+    <!-- Main UI -->
+    <div class="mt-3 mb-3">
+      Submitted Names:
+      <ul>
+        <li v-for="n in names">{{n}}</li>
+      </ul>
+    </div>
+    <!-- Modal Component -->
+    <b-modal id="modalPrevent"
+             ref="modal"
+             title="Submit your name"
+             @ok="handleOk"
+             @shown="clearName">
+      <form @submit.stop.prevent="handleSubmit">
+        <b-form-input type="text"
+                      placeholder="Enter your name"
+                      v-model="name"></b-form-input>
+      </form>
+    </b-modal>
+            </div>
         </div>
     </div>
   
@@ -89,6 +111,37 @@ inbox(){
     
 }
 </script>
+<script>
+export default {
+  data () {
+    return {
+      name: '',
+      names: []
+    }
+  },
+  methods: {
+    clearName () {
+      this.name = ''
+    },
+    handleOk (evt) {
+      // Prevent modal from closing
+      evt.preventDefault()
+      if (!this.name) {
+        alert('Please enter your name')
+      } else {
+        this.handleSubmit()
+      }
+    },
+    handleSubmit () {
+      this.names.push(this.name)
+      this.clearName()
+      this.$refs.modal.hide()
+    }
+  }
+}
+
+</script>
+
 <style lang="stylus" scoped>
 .header{
     height:50px;
