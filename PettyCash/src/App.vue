8429bc01e24638c075a3c43fcbebd1dc70a77b72
Jonathan Ato Markin <jona10711@gmail.com>
2018-07-11 20:54:10 +0100

    login changes

--- PettyCash/src/App.vue
+++ PettyCash/src/App.vue
@@ -1,61 +1,9 @@
 <template>
   <div id="app">
-
-    <div class="ui container">
-  <br>
-  <div class="ui secondary menu">
-    <div class="header item">Brand</div>
-    <a class="active item">
-      Home
-    </a>
-    <a class="item">
-      Messages
-    </a>
-    <a class="item">
-      Friends
-    </a>
-    <div class="right menu">
-      <div class="item">
-        <div class="ui icon input">
-          <input type="text" placeholder="Search...">
-          <i class="search link icon"></i>
-        </div>
-      </div>
-      <a class="ui item">
-        Logout
-      </a>
-    </div>
-  </div>
-  <div class="ui divider"></div>
-  <br>
-  <div class="ui grid">
-    <div class="four wide column">
-      <div class="ui secondary vertical pointing fluid menu">
-        <a class="item">
-          Home
-        </a>
-        <a class="item">
-          Messages
-        </a>
-        <a class="item active">
-          Friends
-        </a>
-      </div>
-    </div>
-    <div class="twelve wide column">
-      <div class="ui form">
-        <div class="field">
-          <input type="text" name="first-name" placeholder="First name">
-        </div>
-        <div class="field">
-          <textarea placeholder="Some example text..."></textarea>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- <div class="ui grid no-margin min-height-100"> -->
+   
+      <router-view/>
+    <!-- </div> -->
   </div>
 </template>
 
@@ -67,11 +15,11 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
---
+++ PettyCash/src/components/Login.vue
@@ -0,0 +1,56 @@
+<template>
+
+    <div class="ui centered internally celled grid loginbox">
+    <div class="row login">
+        <div class="four wide column loginside center aligned ">
+        <img class="ui small centered circular image" src="../assets/img/users/usericon.png">
+        <button class="ui white button rounded z-depth but"><i class="red google icon"></i> Login With Google</button>
+        </div>
+        <div class="six wide column pettyside center aligned">
+            <h1>Welcome</h1>
+            <h3>Login to Access Your Account</h3>
+        </div>
+    </div>
+    </div>
+
+</template>
+
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.loginbox{
+    border: 1px sloid black;
+    height: 600px;;
+    box-shadow: 5px 10px #888888;
+    padding-top:100px;
+    padding-bottom:100px;
+}
+.loginside{
+    background: #f4f4f4;
+}
+
+.pettyside{
+    background:#f1f1f1;
+}
+
+login{
+    margin-top:50px;
+}
+
+.but{
+    background: white;
+    border-radius:15px;
+    border: 1px solid #888888;
+}
+</style>
